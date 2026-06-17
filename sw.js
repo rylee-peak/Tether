9e40dc1cd3c39709e4ac3e10a4b03a986534e170
R. Peak <rylee.peak@thewca.us>
2026-06-17 10:59:47 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 // 2. OFFLINE CACHE CONFIGURATION
 // ==========================================================
 // CHANGE THIS VERSION NUMBER whenever you update your app's HTML/JS!
-const CACHE_NAME = 'tether-app-cache-v10'; 
+const CACHE_NAME = 'tether-app-cache-v11'; 
 
 const STATIC_ASSETS = [
   '/',
