9bdabddaa266d10958fc4692d5aab620014d6228
R. Peak <rylee.peak@thewca.us>
2026-06-16 21:36:09 -0700

    Fixing bug with version update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this version number to force an update for all users!
-const CACHE_NAME = 'tether-app-cache-v8'; 
+const CACHE_NAME = 'tether-app-cache-v9'; 
 
 const STATIC_ASSETS = [
   '/',
